83f4239e1343e2ea40a40176b881860b118cdd4a
Nelson Rosado <nelsonog827@gmail.com>
2021-08-18 15:49:31 -0400

    FINAL. Fixed manifest.json error when building app

---
+++ client/build/precache-manifest.b04ab30b88dd115d52f4aece993c087b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d4ad523b8050c875fa6dfcae2c865f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6984b6087e74e424189",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "736a91e8ed75ceff9110",
+    "url": "/static/js/2.ed136bc1.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.ed136bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6984b6087e74e424189",
+    "url": "/static/js/main.9ec015b4.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
